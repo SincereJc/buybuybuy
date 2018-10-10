4c8c77474608a0f485024018efac21cfa19597e7
SincereJc <531434215@qq.com>
2018-10-10 13:43:06 +0800

    买买买首页完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// 导入element-ui模块
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 导入样式
+import './assets/statics/site/css/style.css';
+// 导入vue-router模块
+import VueRouter from 'vue-router';
+// 导入组件
+import index from './components/index.vue';
+import daily from './components/daily.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+// 定义路由
+const routes = [
+  { path: '/', component: index },
+  { path: '/index', component: index },
+  { path: '/daily', component: daily }
+];
+// 3. 创建 router 实例，然后传 `routes` 配置
+const router = new VueRouter({
+  routes 
+});
+
+
+
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  // 挂载到vue实例
+  router
+}).$mount('#app');
